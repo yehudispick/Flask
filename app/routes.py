00db9c29028d2cb8d7e681d824f61f5d72a64f46
yehudispick <49215806+yehudispick@users.noreply.github.com>
2019-10-18 00:42:13 -0400

    Chapter 6

--- app/routes.py
+++ app/routes.py
@@ -58,7 +58,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
-        user = set_password(form.password.data)
+        user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
