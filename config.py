2500b53a3a3078c2efea30206ccf5d4b814e58d8
yehudis pick <pickyehudis@gmail.com>
2019-10-20 16:33:42 -0400

    Chapter 9

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['pickyehudis@gmail.com']
-    
+    POSTS_PER_PAGE = 3
